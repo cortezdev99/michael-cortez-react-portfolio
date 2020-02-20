34fcefd1aa269a7265167bdaa755a41e491a1892
Michale-Cortez <CortezDevelopement@gmail.com>
2020-02-20 16:28:48 -0700

    Added Portfolio Container

---
+++ src/components/portfolio-container.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+export default class PortfolioContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h2>
+          Portfolio items go here..
+        </h2>
+      </div>
+    )
+  }
+}
