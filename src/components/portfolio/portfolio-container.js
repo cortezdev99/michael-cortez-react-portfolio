cf6454536c12fe9bfca9df276b5efe6ffecff36a
Michale-Cortez <CortezDevelopement@gmail.com>
2020-02-24 14:42:45 -0700

    Added conditional for rendering content when the page has or hasnt loaded yet

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -6,6 +6,7 @@ export default class PortfolioContainer extends Component {
     super()
     this.state = {
       pageTitle: "Welcome to my portfolio",
+      isLoading: true,
       data: [
         { title: "Quip", catagory: "eCommerce" },
         { title: "Evenetbrite", catagory: "Scheduling" },
@@ -32,15 +33,24 @@ export default class PortfolioContainer extends Component {
   }
 
   render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>
+    }
     return (
       <div>
         <h2>
           {this.state.pageTitle}
         </h2>
 
-        <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
-        <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-        <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
+        <button onClick={() => this.handleFilter('eCommerce')}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter('Scheduling')}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter('Enterprise')}>
+          Enterprise
+        </button>
 
         {this.portfolioItems()}
       </div>
