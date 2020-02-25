38adc7259c93eec365e5495ca68de74972458585
Michale-Cortez <CortezDevelopement@gmail.com>
2020-02-25 10:06:12 -0700

    Added a component to dynamicaly add links to each project item

--- src/components/app.js
+++ src/components/app.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import PortfolioContainer from "./portfolio/portfolio-container"
 import NavigationContainer from "./navigation/navigation-container"
 import Home from "./pages/home"
 import About from "./pages/about"
 import Contact from "./pages/contact"
 import Blog from "./pages/blog"
-import contact from './pages/contact';
+import PortfolioDetail from "./portfolio/portfolio-detail"
 
 export default class App extends Component {
   render() {
@@ -16,6 +15,10 @@ export default class App extends Component {
       <div className='app'>
         <Router>
           <div>
+            <h1>Michael Cortez Portfolio</h1>
+            <div>
+              {moment().format('MMMM Do YYYY, h:mm:ss a')}
+            </div>
             <NavigationContainer />
 
             <Switch>
@@ -23,15 +26,11 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
+              <Route path="/portfolio/:slug" component={PortfolioDetail} />
             </Switch>
           </div>
         </Router>
 
-        <h1>Michael Cortez Portfolio</h1>
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
-        <PortfolioContainer />
       </div>
     );
   }
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -8,10 +8,10 @@ export default class PortfolioContainer extends Component {
       pageTitle: "Welcome to my portfolio",
       isLoading: false,
       data: [
-        { title: "Quip", catagory: "eCommerce" },
-        { title: "Evenetbrite", catagory: "Scheduling" },
-        { title: "Ministry Safe", catagory: "Enterprise" },
-        { title: "SwingAway", catagory: "eCommerce" }
+        { title: "Quip", catagory: "eCommerce", slug: 'quip' },
+        { title: "Evenetbrite", catagory: "Scheduling", slug: 'eventbrite' },
+        { title: "Ministry Safe", catagory: "Enterprise", slug: 'ministry-safe' },
+        { title: "SwingAway", catagory: "eCommerce", slug: 'swingaway' }
       ]
     };
 
@@ -28,7 +28,7 @@ export default class PortfolioContainer extends Component {
 
   portfolioItems() {
     return this.state.data.map(item => {
-      return <PortfolioItem title={item.title} url={"google.com"} />
+      return <PortfolioItem title={item.title} url={"google.com"} slug={item.slug}/>
     })
   }
 
