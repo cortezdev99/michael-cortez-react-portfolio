66a6ba56cc8dab4a0d3d89230e0da67eb24ec9bf
Michale-Cortez <CortezDevelopement@gmail.com>
2020-02-26 11:59:14 -0700

    Building the Navigation Link Wrapper Styles, also added a vs-code json file so all my javascript files will always be react

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -5,7 +5,7 @@ export default function(props) {
   const { id, description, thumb_image_url, logo } = props.item;
   return (
     <div>
-      {/* <img src={thumb_image_url} /> */}
+      <img src={thumb_image_url} />
       <img src={logo} />
 
 
