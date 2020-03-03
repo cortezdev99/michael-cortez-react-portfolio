f140bcd84fb4d70b2ec6b5ec78615e69b10b5f58
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-03 14:03:38 -0700

    How to Delete API Data in React and Update State

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -17,7 +17,17 @@ export default class PortfolioManager extends Component {
   }
 
   handleDeleteClick(portfolioItem) {
-    console.log("handleDeleteClick", portfolioItem)
+    axios.delete(`https://api.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`, { withCredentials: true }).then(response => {
+      this.setState({
+        portfolioItems: this.state.portfolioItems.filter(item => {
+          return item.id !== portfolioItem.id;
+        })
+      })
+
+      return response.data;
+    }).catch(error => {
+      console.log('error in handleDeleteClick', error)
+    })
   }
 
   handleSuccessfulFormSubmission(portfolioItem) {
