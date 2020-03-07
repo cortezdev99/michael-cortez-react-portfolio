28bf47c2efeb2f79fe1ca1635be622d51c94ce52
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-07 13:02:35 -0700

    Building the Blog Form Event Handlers

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -21,6 +21,11 @@ export default class BlogModal extends Component {
         backgroundColor: "rgba(1, 1, 1, 0.75"
       }
     }
+    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this)
+  }
+
+  handleSuccessfulFormSubmission(blog) {
+    console.log("blog from blog form", blog)
   }
 
   render() {
@@ -32,7 +37,7 @@ export default class BlogModal extends Component {
       }} 
         isOpen={this.props.modalIsOpen}
       >
-        <BlogForm />
+        <BlogForm handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}/>
       </ReactModal>
     )
   }
