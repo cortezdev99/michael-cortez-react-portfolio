236b05d6a9190e9e53d72cc08113ff237893a063
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-06 17:34:18 -0700

    Added my modal and gave it styles

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -4,11 +4,32 @@ import ReactModal from 'react-modal'
 export default class BlogModal extends Component {
   constructor(props) {
     super(props)
+
+    this.customStyles = {
+      content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+        width: "800px"
+      },
+      overlay: {
+        backgroundColor: "rgba(1, 1, 1, 0.75"
+      }
+    }
   }
 
   render() {
     return (
-      <ReactModal isOpen={true}>
+      <ReactModal
+        style={this.customStyles}
+        onRequestClose={() => {
+        this.props.handleModalClose()
+      }} 
+        isOpen={this.props.modalIsOpen}
+        ariaHideApp={false}
+      >
         <h1>I'm in a modal!</h1>
       </ReactModal>
     )
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -12,14 +12,29 @@ export default class Blog extends Component {
       blogItems: [],
       totalCount: 0,
       currentPage: 0,
-      isLoading: true
+      isLoading: true,
+      blogModalIsOpen: false
     }
 
     this.getBlogItems = this.getBlogItems.bind(this)
     this.onScroll = this.onScroll.bind(this)
     window.addEventListener("scroll", this.onScroll, false)
+    this.handleNewBlogClick = this.handleNewBlogClick.bind(this)
+    this.handleModalClose = this.handleModalClose.bind(this)
   }
 
+  handleNewBlogClick() {
+    this.setState({
+      blogModalIsOpen: true
+    })
+  }
+
+  handleModalClose() {
+    this.setState({
+      blogModalIsOpen: false
+    })
+  }
+  
   onScroll() {
     if ( this.state.isLoading || this.state.blogItems.length === this.state.totalCount) {
       return;
@@ -63,8 +78,17 @@ export default class Blog extends Component {
 
     return (
       <div className="blog-container">
-        <BlogModal />
-        
+        <BlogModal
+          modalIsOpen={this.state.blogModalIsOpen}
+          handleModalClose={this.handleModalClose}
+        />
+
+        <div className="new-blog-link">
+          <a onClick={this.handleNewBlogClick}>
+            Open Modal!
+          </a>
+        </div>
+
         <div className="content-container">
           {blogRecords}
         </div>
