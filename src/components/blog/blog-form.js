7f8ebba206664c687d571dc88bdaca9c8570e4fe
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-09 14:29:52 -0600

    Configuring Dropzone for Blog Featured Image

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import DropzoneComponent from 'react-dropzone-component'
 
 import RichTextEditor from "../forms/rich-text-editor"
 
@@ -10,12 +11,39 @@ export default class BlogForm extends Component {
     this.state = {
       title: "",
       blog_status: "",
-      content: ""
+      content: "",
+      featured_image: ""
     }
 
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(this)
+    this.componentConfig = this.componentConfig.bind(this)
+    this.djsConfig = this.djsConfig.bind(this)
+    this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(this)
+  }
+
+  handleFeaturedImageDrop() {
+    return {
+      addedfile: file => this.setState({
+        featured_image: file
+      })
+    }
+  }
+
+  djsConfig() {
+    return {
+      addRemoveLinks: true,
+      maxFiles: 1
+    }
+  }
+
+  componentConfig() {
+    return {
+      iconFiletypes: [".jpg", ".png"],
+      showFiletypeIcon: true,
+      postUrl: "https://httpbin.org/post"
+    }
   }
 
   handleRichTextEditorChange(content) {
@@ -75,6 +103,16 @@ export default class BlogForm extends Component {
         </div>
           <div className="one-column">
             <RichTextEditor handleRichTextEditorChange={this.handleRichTextEditorChange}/>
+          </div>
+
+          <div className="image-uploaders">
+            <DropzoneComponent
+              config={this.componentConfig()}
+              djsConfig={this.djsConfig()}
+              eventHandlers={this.handleFeaturedImageDrop()}
+            >
+              <div className="dz-message">Featured Image</div>
+            </DropzoneComponent>
           </div>
 
           <button className="btn one-column">Save</button>
