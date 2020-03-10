cba430869fdf9a17c2b14d0736b0e8a4c6114835
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-10 14:21:18 -0600

    Enabling Authorization Rules for Blog Detail Component

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -35,9 +35,11 @@ export default class BlogDetail extends Component {
   }
 
   handleEditClick() {
-    this.setState({
-      editMode: true
-    })
+    if (this.props.loggedInStatus === "LOGGED_IN") {
+      this.setState({
+        editMode: true
+      })
+    }
   }
 
   getBlogItem() {
