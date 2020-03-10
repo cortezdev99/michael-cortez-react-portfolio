1943070f41e708bd6ce045ef53152fe698f32cfe
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-10 15:05:41 -0600

    Building About Page Styles for React Portfolio

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import profilePicture from "../../../static/assets/images/bio/headshot.jpg"
 
 export default function () {
   return (
-    <div>
-      About
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + profilePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }}
+      />
+
+      <div className="right-column">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae et at aut ut. Dolor nisi, quidem, est aspernatur aperiam numquam odio molestias omnis beatae temporibus accusantium! Reiciendis consequuntur consectetur praesentium.
+        Asperiores libero dolorum ducimus voluptates delectus voluptatibus tempore animi cum ipsam optio ut tempora ea voluptatem vero molestias, reprehenderit aperiam non fugiat, sequi placeat numquam? Aut placeat aspernatur quia magnam?
+        Cum labore enim a reiciendis nulla voluptatibus atque unde, minus, reprehenderit suscipit aut sunt quaerat ducimus recusandae eligendi officiis magni quas omnis. Sequi nisi id a minus fugit. Veritatis, suscipit.
+        Commodi voluptates omnis, unde aliquam ea voluptatum voluptas sequi cum, qui suscipit recusandae quaerat, adipisci facere magni dolores animi iusto dolore quam ratione doloremque nobis explicabo provident. Tempore, assumenda nostrum.
+        Aperiam ipsa impedit perferendis, minus placeat quibusdam vel dolorem sed totam reprehenderit ea, autem, minima magni doloribus quas facilis. Dolore quos quaerat nemo necessitatibus sunt sit ipsum! Perferendis, modi. Excepturi.
+        Illo, praesentium ab! Soluta ab quae, debitis rerum maiores earum sequi omnis adipisci consequatur nesciunt iure voluptate, dolores deleniti minus cumque dicta magni voluptatibus. Sint aspernatur minus architecto ipsam consectetur.
+        Quos iste voluptatum mollitia iure illo natus quis, minus, placeat maiores repellendus vero at omnis doloremque totam error? Saepe repellat reiciendis maiores quia. Cum dolor ducimus eaque et eligendi error?
+        Mollitia sit nam rem, corrupti quisquam assumenda consequuntur repellendus asperiores amet labore dolorem possimus, quos tenetur, accusamus impedit similique odio obcaecati alias at ipsam quasi vero. Animi aspernatur obcaecati praesentium.
+      </div>
     </div>
   )
 }
