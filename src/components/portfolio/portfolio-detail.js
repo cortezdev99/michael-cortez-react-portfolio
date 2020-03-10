ec74d766e990d0b965ef126f03f112166e4d418d
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-10 15:41:02 -0600

    Rendering the Portfolio Detail Data on the Screen

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -4,6 +4,10 @@ import axios from 'axios'
 export default class PortfolioDetail extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      portfolioItem: {}
+    }
   }
 
   componentDidMount() {
@@ -12,16 +16,29 @@ export default class PortfolioDetail extends Component {
 
   getPortfolioItem() {
     axios.get(`https://michaelcortez.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`, { withCredentials: true }).then(response => {
-      console.log("res", response)
+      this.setState({
+        portfolioItem: response.data.portfolio_item
+      })
     }).catch(error => {
       console.log("getPortfolioItem err", error)
     })
   }
 
   render() {
+    const {
+      banner_image_url,
+      category,
+      description,
+      logo_url,
+      name,
+      thumb_image_url,
+      url
+    } = this.state.portfolioItem
+
     return (
       <div>
-        <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+        <h2>{name}</h2>
+        <p>{description}</p>
       </div>
     )
   }
