66703c3fdbb3d90e30a65a4efd22ca31521ba0cd
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-10 15:33:22 -0600

    Initial Build Out of the Portfolio Detail Component

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-export default function (props) {
-  return (
-    <div>
-      <h2>Portfolio Detail for {props.match.params.slug}</h2>
-    </div>
-  )
+export default class PortfolioDetail extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    this.getPortfolioItem()
+  }
+
+  getPortfolioItem() {
+    axios.get(`https://michaelcortez.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`, { withCredentials: true }).then(response => {
+      console.log("res", response)
+    }).catch(error => {
+      console.log("getPortfolioItem err", error)
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+      </div>
+    )
+  }
 }
