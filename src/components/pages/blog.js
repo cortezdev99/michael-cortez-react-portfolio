1f6ad66ddc7ab06d2916d349041cd778b6e13749
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-10 16:43:07 -0600

    Finalizing the Auth Form Styles

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -22,6 +22,21 @@ export default class Blog extends Component {
     this.handleNewBlogClick = this.handleNewBlogClick.bind(this)
     this.handleModalClose = this.handleModalClose.bind(this)
     this.handleSuccessfulNewBlogSubmission  = this.handleSuccessfulNewBlogSubmission.bind(this)
+    this.handleDeleteClick = this.handleDeleteClick.bind(this)
+  }
+
+  handleDeleteClick(blog) {
+    axios.delete(`https://api.devcamp.space/portfolio/portfolio_blogs/${blog.id}`, { withCredentials: true }).then(response => {
+      this.setState({
+        blogItems: this.state.blogItems.filter(blogItem => {
+          return blog.id !== blogItem.id
+        })
+      })
+
+      return response.data
+    }).catch(error => {
+      console.log("handleDeleteClick error", error)
+    })
   }
 
   handleSuccessfulNewBlogSubmission(blog) {
@@ -81,7 +96,16 @@ export default class Blog extends Component {
 
   render() {
     const blogRecords = this.state.blogItems.map(blogItem => {
-      return <BlogItem key={blogItem.id} blogItem={blogItem} />
+      if (this.props.loggedInStatus === "LOGGED_IN") {
+        return (
+          <div className="admin-blog-wrapper" key={blogItem.id}>
+            <BlogItem blogItem={blogItem} />
+            <a onClick={() => this.handleDeleteClick(blogItem)}><FontAwesomeIcon icon="trash" /></a>
+          </div>
+        )
+      } else {
+        return <BlogItem key={blogItem.id} blogItem={blogItem} />
+      }
     })
 
     return (
